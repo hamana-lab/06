fc2555f58748c1fc760c5c945fb9eaa4b12de21c
hamana-lab <hamana@sookmyung.ac.kr>
2025-10-15 20:54:54 +0900

    week06_2-1

--- main.c
+++ main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+void func2(void)
+{
+     int x;
+     printf("func2 x is at %p\n", &x);
+      
+}
+
+
 void func(void)
 {
      int x;
      printf("func x is at %p\n", &x);
-      
+     func2(); 
 }
 
 
@@ -13,6 +21,7 @@ int main(void)
   int x;
   printf("main x is at %p\n", &x);
   func();
+
   
   system("PAUSE");	
   return 0;
