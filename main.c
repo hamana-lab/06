cc8a70d4e3198d37a78fa3232e6eee431b64d137
hamana-lab <hamana@sookmyung.ac.kr>
2025-10-15 21:48:12 +0900

    week06_5

--- main.c
+++ main.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int square(int a)
+
+int factorial(int n)
 {
-  return(a*a);
+    int res = 1;
+    int i;
+    for(i=1;i<=n;i++)
+     res = res*i;
+     
+    return res;   
 }
+
+int combination(int n, int r)
+{
+    return factorial(n)/factorial(n-r)/factorial(r);
+}
+
 
 int main()
 {
-  int a = 2;
-  a = square(a);
-  printf("a=%i\n", a);
+    int n, r;
+    int res;
+    
+    printf("Input n and r: ");
+    scanf("%d %d", &n, &r);
+    
+    res = combination(n, r);
+    
+    printf("combination result is : %i\n", res);
 
-  system("PAUSE");	
+    system("PAUSE");	
+    return 0;
 }
